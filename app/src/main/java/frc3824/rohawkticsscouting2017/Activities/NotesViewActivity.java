5403a7de6be29792a6ddf4ebd285a27eecb5e140
amessing <amessing@utk.edu>
2016-09-14 12:54:12 -0400

    Split advanced search into multiple functions

--- app/src/main/java/frc3824/rohawkticsscouting2017/Activities/NotesViewActivity.java
+++ app/src/main/java/frc3824/rohawkticsscouting2017/Activities/NotesViewActivity.java
@@ -31,6 +31,7 @@ public class NotesViewActivity extends Activity implements View.OnClickListener 
 
     private final static String TAG = "NotesViewActivity";
 
+    //region variables
     private RelativeLayout mBasicSearch;
     private Button mBasicSearchOpen;
     private RadioGroup mBasicSearchType;
@@ -55,6 +56,7 @@ public class NotesViewActivity extends Activity implements View.OnClickListener 
     private ArrayList<NoteView> mFilteredNotes;
 
     private LayoutInflater mLayoutInflator;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -102,6 +104,7 @@ public class NotesViewActivity extends Activity implements View.OnClickListener 
         switch (view.getId())
         {
             case R.id.basic_search_open:
+                //region Basic Search Open
                 mBasicSearch.setVisibility(View.VISIBLE);
                 mAdvancedSearch.setVisibility(View.GONE);
                 mBasicSearchOpen.setVisibility(View.GONE);
@@ -119,12 +122,15 @@ public class NotesViewActivity extends Activity implements View.OnClickListener 
                 {
                     mAdvancedSearchContentCriteria.removeViewAt(0);
                 }
+                //endregion
                 break;
             case R.id.advanced_search_open:
+                //region Advanced Search Open
                 mBasicSearch.setVisibility(View.GONE);
                 mAdvancedSearch.setVisibility(View.VISIBLE);
                 mBasicSearchOpen.setVisibility(View.VISIBLE);
                 mAdvancedSearchOpen.setVisibility(View.GONE);
+                //endregion
                 break;
             case R.id.basic_search_button:
                 break;
@@ -138,136 +144,175 @@ public class NotesViewActivity extends Activity implements View.OnClickListener 
                 mAdvancedSearchContentCriteria.addView(new NoteCriteriaContent(this), 0);
                 break;
             case R.id.advanced_search_button:
-
-                // Remove all the "Gone" views
-                for(int i = 0; i < mAdvancedSearchMatchNumberCriteria.getChildCount(); i++)
-                {
-                    if(mAdvancedSearchMatchNumberCriteria.getChildAt(i).getVisibility() == View.GONE)
-                    {
-                        mAdvancedSearchMatchNumberCriteria.removeViewAt(i);
-                        i--;
-                    }
-                }
-                for(int i = 0; i < mAdvancedSearchTeamNumberCriteria.getChildCount(); i++)
-                {
-                    if(mAdvancedSearchTeamNumberCriteria.getChildAt(i).getVisibility() == View.GONE)
-                    {
-                        mAdvancedSearchTeamNumberCriteria.removeViewAt(i);
-                        i--;
-                    }
-                }
-                for(int i = 0; i < mAdvancedSearchContentCriteria.getChildCount(); i++)
-                {
-                    if(mAdvancedSearchContentCriteria.getChildAt(i).getVisibility() == View.GONE)
-                    {
-                        mAdvancedSearchContentCriteria.removeViewAt(i);
-                        i--;
-                    }
-                }
-
-                ArrayList<Integer[]> matchRanges = new ArrayList<>();
-                for(int i = 0; i < mAdvancedSearchMatchNumberCriteria.getChildCount() - 1; i++)
-                {
-                    NoteCriteriaNumber ncn = (NoteCriteriaNumber)mAdvancedSearchMatchNumberCriteria.getChildAt(i);
-                    switch (ncn.getType())
-                    {
-                        //Between
-                        case 0:
-                            matchRanges.add(new Integer[]{ncn.getBefore(), ncn.getAfter()});
-                            break;
-                        //Before
-                        case 1:
-                            matchRanges.add(new Integer[]{ncn.getBefore(), -1});
-                            break;
-                        //After
-                        case 2:
-                            matchRanges.add(new Integer[]{-1, ncn.getAfter()});
-                            break;
-                    }
-                }
-                ArrayList<Integer[]> teamRanges = new ArrayList<>();
-                for(int i = 0; i < mAdvancedSearchTeamNumberCriteria.getChildCount() - 1; i++)
-                {
-                    NoteCriteriaNumber ncn = (NoteCriteriaNumber)mAdvancedSearchTeamNumberCriteria.getChildAt(i);
-                    switch (ncn.getType())
-                    {
-                        //Between
-                        case 0:
-                            teamRanges.add(new Integer[]{ncn.getBefore(), ncn.getAfter()});
-                            break;
-                        //Before
-                        case 1:
-                            teamRanges.add(new Integer[]{ncn.getBefore(), Integer.MAX_VALUE});
-                            break;
-                        //After
-                        case 2:
-                            teamRanges.add(new Integer[]{Integer.MIN_VALUE, ncn.getAfter()});
-                            break;
-                    }
-                }
-
-                ArrayList<String> contains = new ArrayList<>();
-                for(int i = 0; i < mAdvancedSearchContentCriteria.getChildCount() - 1; i++)
-                {
-                    contains.add(((NoteCriteriaContent)mAdvancedSearchContentCriteria.getChildAt(i)).getContains());
-                }
-                mFilteredNotes = new ArrayList<>(mAllNotes);
-                for(int i = 0; i < mFilteredNotes.size(); i++)
-                {
-                    NoteView nv = mFilteredNotes.get(i);
-                    boolean remove = true;
-                    for(int j = 0; j < matchRanges.size(); j++)
-                    {
-                        int before = matchRanges.get(j)[0];
-                        int after = matchRanges.get(j)[1];
-                        if(nv.match_number >= before && nv.match_number <= after)
-                        {
-                            remove = false;
-                            break;
-                        }
-                    }
-                    if(remove)
-                    {
-                        mFilteredNotes.remove(i);
-                        i--;
-                        continue;
-                    }
-                    remove = true;
-                    for(int j = 0; j < teamRanges.size(); j++)
-                    {
-                        int before = teamRanges.get(j)[0];
-                        int after = teamRanges.get(j)[1];
-                        if(nv.team_number >= before && nv.team_number <= after)
-                        {
-                            remove = false;
-                            break;
-                        }
-                    }
-                    if(remove)
-                    {
-                        mFilteredNotes.remove(i);
-                        i--;
-                        continue;
-                    }
-                    remove = true;
-                    for(int j = 0; j < contains.size(); j++)
-                    {
-                        if(nv.note.contains(contains.get(j)))
-                        {
-                            remove = false;
-                            break;
-                        }
-                    }
-                    if(remove)
-                    {
-                        mFilteredNotes.remove(i);
-                        i--;
-                        continue;
-                    }
-                }
-                mAdapter = new LVA_NotesView(this, mFilteredNotes);
-                mListView.setAdapter(mAdapter);
+                advancedSearch();
                 break;
         }
     }
+
+    private void advancedSearch()
+    {
+        cleanGoneViews();
+
+        ArrayList<Integer[]> matchRanges = getMatchRanges();
+        ArrayList<Integer[]> teamRanges = getTeamRanges();
+
+        ArrayList<String> contains = new ArrayList<>();
+        for(int i = 0; i < mAdvancedSearchContentCriteria.getChildCount() - 1; i++)
+        {
+            contains.add(((NoteCriteriaContent)mAdvancedSearchContentCriteria.getChildAt(i)).getContains());
+        }
+
+        mFilteredNotes = new ArrayList<>(mAllNotes);
+        for(int i = 0; i < mFilteredNotes.size(); i++)
+        {
+            NoteView nv = mFilteredNotes.get(i);
+
+            if(filterMatches(nv, matchRanges))
+            {
+                mFilteredNotes.remove(i);
+                i--;
+                continue;
+            }
+
+
+            if(filterTeams(nv, teamRanges))
+            {
+                mFilteredNotes.remove(i);
+                i--;
+                continue;
+            }
+
+            if(filterContents(nv, contains))
+            {
+                mFilteredNotes.remove(i);
+                i--;
+                continue;
+            }
+        }
+        mAdapter = new LVA_NotesView(this, mFilteredNotes);
+        mListView.setAdapter(mAdapter);
+    }
+
+    private void cleanGoneViews()
+    {
+        // Remove all the "Gone" views
+        for(int i = 0; i < mAdvancedSearchMatchNumberCriteria.getChildCount(); i++)
+        {
+            if(mAdvancedSearchMatchNumberCriteria.getChildAt(i).getVisibility() == View.GONE)
+            {
+                mAdvancedSearchMatchNumberCriteria.removeViewAt(i);
+                i--;
+            }
+        }
+        for(int i = 0; i < mAdvancedSearchTeamNumberCriteria.getChildCount(); i++)
+        {
+            if(mAdvancedSearchTeamNumberCriteria.getChildAt(i).getVisibility() == View.GONE)
+            {
+                mAdvancedSearchTeamNumberCriteria.removeViewAt(i);
+                i--;
+            }
+        }
+        for(int i = 0; i < mAdvancedSearchContentCriteria.getChildCount(); i++)
+        {
+            if(mAdvancedSearchContentCriteria.getChildAt(i).getVisibility() == View.GONE)
+            {
+                mAdvancedSearchContentCriteria.removeViewAt(i);
+                i--;
+            }
+        }
+    }
+
+    //region get ranges
+    private ArrayList<Integer[]> getMatchRanges()
+    {
+        ArrayList<Integer[]> rv = new ArrayList<>();
+        for(int i = 0; i < mAdvancedSearchMatchNumberCriteria.getChildCount() - 1; i++)
+        {
+            NoteCriteriaNumber ncn = (NoteCriteriaNumber)mAdvancedSearchMatchNumberCriteria.getChildAt(i);
+            switch (ncn.getType())
+            {
+                //Between
+                case 0:
+                    rv.add(new Integer[]{ncn.getBefore(), ncn.getAfter()});
+                    break;
+                //Before
+                case 1:
+                    rv.add(new Integer[]{ncn.getBefore(), -1});
+                    break;
+                //After
+                case 2:
+                    rv.add(new Integer[]{-1, ncn.getAfter()});
+                    break;
+            }
+        }
+        return rv;
+    }
+
+    private ArrayList<Integer[]> getTeamRanges()
+    {
+        ArrayList<Integer[]> rv = new ArrayList<>();
+        for(int i = 0; i < mAdvancedSearchTeamNumberCriteria.getChildCount() - 1; i++)
+        {
+            NoteCriteriaNumber ncn = (NoteCriteriaNumber)mAdvancedSearchTeamNumberCriteria.getChildAt(i);
+            switch (ncn.getType())
+            {
+                //Between
+                case 0:
+                    rv.add(new Integer[]{ncn.getBefore(), ncn.getAfter()});
+                    break;
+                //Before
+                case 1:
+                    rv.add(new Integer[]{ncn.getBefore(), -1});
+                    break;
+                //After
+                case 2:
+                    rv.add(new Integer[]{-1, ncn.getAfter()});
+                    break;
+            }
+        }
+        return rv;
+    }
+    //endregion
+
+    //region filters
+    private boolean filterMatches(NoteView nv, ArrayList<Integer[]> matchRanges)
+    {
+        for(int j = 0; j < matchRanges.size(); j++)
+        {
+            int before = matchRanges.get(j)[0];
+            int after = matchRanges.get(j)[1];
+            if(nv.match_number >= before && nv.match_number <= after)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean filterTeams(NoteView nv, ArrayList<Integer[]> teamRanges)
+    {
+        for(int j = 0; j < teamRanges.size(); j++)
+        {
+            int before = teamRanges.get(j)[0];
+            int after = teamRanges.get(j)[1];
+            if(nv.team_number >= before && nv.team_number <= after)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean filterContents(NoteView nv, ArrayList<String> contains)
+    {
+        for(int j = 0; j < contains.size(); j++)
+        {
+            if(nv.note.contains(contains.get(j)))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    //endregion
 }
