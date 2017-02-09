acd163eb4a7edfd735d158dbe7dbb360c31bd187
amessing <amessing@utk.edu>
2017-02-09 17:25:54 -0500

    Fixed data model

--- app/src/main/java/frc3824/rohawkticsscouting2017/Firebase/DataModels/TeamPitData.java
+++ app/src/main/java/frc3824/rohawkticsscouting2017/Firebase/DataModels/TeamPitData.java
@@ -45,7 +45,7 @@ public class TeamPitData {
     public String programming_language;
     public String drive_train;
     public int cims;
-    public float max_hopper_load;
+    public double max_hopper_load;
     public String chosen_volume;
 
 
@@ -55,6 +55,7 @@ public class TeamPitData {
     public TeamPitData(){
         robot_image_filepaths = new ArrayList<>();
         robot_image_urls = new ArrayList<>();
+        robot_image_default = -1;
     }
 
     public TeamPitData(ScoutMap map) {
@@ -64,6 +65,8 @@ public class TeamPitData {
 
             if(map.contains(Constants.Pit_Scouting.ROBOT_PICTURE_DEFAULT)){
                 robot_image_default = map.getInt(Constants.Pit_Scouting.ROBOT_PICTURE_DEFAULT);
+            } else {
+                robot_image_default = -1;
             }
 
             robot_image_filepaths = (ArrayList)map.getObject(Constants.Pit_Scouting.ROBOT_PICTURE_FILEPATHS);
@@ -79,7 +82,7 @@ public class TeamPitData {
             programming_language = map.getString(Constants.Pit_Scouting.Miscellaneous.PROGRAMMING_LANGUAGE);
             cims = map.getInt(Constants.Pit_Scouting.Miscellaneous.CIMS);
             drive_train = map.getString(Constants.Pit_Scouting.Miscellaneous.DRIVE_TRAIN);
-            max_hopper_load = (float)map.getDouble(Constants.Pit_Scouting.Miscellaneous.MAX_HOPPER_LOAD);
+            max_hopper_load = map.getDouble(Constants.Pit_Scouting.Miscellaneous.MAX_HOPPER_LOAD);
             chosen_volume = map.getString(Constants.Pit_Scouting.Miscellaneous.CHOSEN_VOLUME);
 
             notes = map.getString(Constants.Pit_Scouting.NOTES);
@@ -107,6 +110,9 @@ public class TeamPitData {
         map.put(Constants.Pit_Scouting.Dimensions.WEIGHT, weight);
 
         map.put(Constants.Pit_Scouting.Miscellaneous.PROGRAMMING_LANGUAGE, programming_language);
+        map.put(Constants.Pit_Scouting.Miscellaneous.DRIVE_TRAIN, drive_train);
+        map.put(Constants.Pit_Scouting.Miscellaneous.CIMS, cims);
+        map.put(Constants.Pit_Scouting.Miscellaneous.CHOSEN_VOLUME, chosen_volume);
 
         map.put(Constants.Pit_Scouting.NOTES, notes);
 
